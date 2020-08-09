386fe3e4b539175ecd033a326e1799dddeb86453
vzpul <42790878+znkzz@users.noreply.github.com>
2020-08-09 21:04:51 +0800

    Add annotate

--- C/P001-CALLBACK.c
+++ C/P001-CALLBACK.c
@@ -1,6 +1,6 @@
 double Foo(float bar1,float bar2); 
 int main(){
-	double (*bar)(float,float)=Foo;
+	double (*bar)(float,float)/*没有=Foo，就是UB！*/=Foo;
 	return (int)bar(1.2,9.66);
 } 
 double Foo(float bar1,float bar2){

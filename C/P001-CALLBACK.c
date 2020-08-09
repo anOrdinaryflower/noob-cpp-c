c4316018bc2d9ff07c7bf776c1d97af41f19fc0f
vzpul <42790878+znkzz@users.noreply.github.com>
2020-08-09 20:46:45 +0800

    Fix bugs

--- C/P001-CALLBACK.c
+++ C/P001-CALLBACK.c
@@ -1,6 +1,7 @@
+double Foo(float bar1,float bar2); 
 int main(){
-	int (*z)(float,float);
-	return z(1.2,9.66);
+	double (*bar)(float,float)=Foo;
+	return (int)bar(1.2,9.66);
 } 
 double Foo(float bar1,float bar2){
 	return bar1+bar2;
